d64e19ceda4e9e76a7af31b42e4194b581d0220c
asemfaqeh <technicalasem@gmail.com>
2021-03-23 22:03:39 +0200

    first commit

--- src/main/java/com/af/arabimotors/ArabimotorsApplication.java
+++ src/main/java/com/af/arabimotors/ArabimotorsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ArabimotorsApplication {
 
+	//new github
 	public static void main(String[] args) {
 		SpringApplication.run(ArabimotorsApplication.class, args);
 	}
